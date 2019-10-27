73900f42e6fa12f75dae10121592beada92d4c46
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-26 21:47:05 -0700

    Project scaffolding with aws-amplify and aws-amplify-angular

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,10 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
